fe81121f19049e24c2f73883bbb590740c493648
AndhikaRizky04 <112884577+AndhikaRizky04@users.noreply.github.com>
2026-03-11 12:17:48 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1051,7 +1051,7 @@ with tab_about:
 
     # Metric cards
     mc_vals = [("96.9%","Accuracy"),("90.3%","Precision"),("93.3%","Recall"),
-               ("91.8%","F1-Score"),("99.91%","AUC-ROC")]
+               ("91.8%","F1-Score"),("99.9%","AUC-ROC")]
     mc_html = "".join([f"""<div class="mcard">
         <div class="mcard-val">{v}</div><div class="mcard-lbl">{l}</div>
     </div>""" for v,l in mc_vals])
